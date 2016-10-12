b66b6b049dff5eb724fadfcd5a4921193d8c833b
Anil Kumar T V <anilkumar.tv@exilant.com>
2016-10-12 15:42:36 +0530

    Xib changes. converted to swift 3

--- clockHand/ViewController.swift
+++ clockHand/ViewController.swift
@@ -10,19 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet private var circle:hand!
+    @IBOutlet fileprivate var circle:hand!
+    @IBOutlet var label : UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         circle.layer.cornerRadius = 150
         
-        circle.layer.shadowOffset = CGSizeMake(5, 5)
+        circle.layer.shadowOffset = CGSize(width: 5, height: 5)
         circle.layer.shadowOpacity = 0.7
         circle.layer.shadowRadius = 5
-        circle.layer.shadowColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).CGColor
+        circle.layer.shadowColor = UIColor(red: 44.0/255.0, green: 62.0/255.0, blue: 80.0/255.0, alpha: 1.0).cgColor
         
         // Applying borders
-        circle.layer.borderColor = UIColor(red: 80.0/255.0, green: 20.0/255.0, blue: 60.0/255.0, alpha: 1.0).CGColor
+        circle.layer.borderColor = UIColor(red: 80.0/255.0, green: 20.0/255.0, blue: 60.0/255.0, alpha: 1.0).cgColor
         circle.layer.borderWidth = 5.0
         /*
         // Bankground images
@@ -32,34 +33,37 @@ class ViewController: UIViewController {
         */
         
         let r:CGFloat = 10.0
-        let c:CGPoint = CGPointMake(150.0-r, 150.0-r)
-        let strokeColor: UIColor = UIColor.blackColor()
-        let fillColor: UIColor = UIColor.redColor()
+        let c:CGPoint = CGPoint(x: 150.0-r, y: 150.0-r)
+        let strokeColor: UIColor = UIColor.black
+        let fillColor: UIColor = UIColor.red
         let centreCircle = CAShapeLayer()
         centreCircle.frame = CGRect(origin:c, size:CGSize(width:2*r, height:2*r))
-        centreCircle.path = UIBezierPath(ovalInRect:centreCircle.bounds).CGPath
-        centreCircle.fillColor = fillColor.CGColor
-        centreCircle.strokeColor = strokeColor.CGColor
-        centreCircle.fillColor = fillColor == UIColor.clearColor() ? nil : fillColor.CGColor
+        centreCircle.path = UIBezierPath(ovalIn:centreCircle.bounds).cgPath
+        centreCircle.fillColor = fillColor.cgColor
+        centreCircle.strokeColor = strokeColor.cgColor
+        centreCircle.fillColor = fillColor == UIColor.clear ? nil : fillColor.cgColor
         circle.layer.addSublayer(centreCircle)
         
-        _ = NSTimer.scheduledTimerWithTimeInterval(1.0/30.0, target: self, selector: #selector(ViewController.rotateHand), userInfo: nil, repeats: true)
+        _ = Timer.scheduledTimer(timeInterval: 1.0/30.0, target: self, selector: #selector(ViewController.rotateHand), userInfo: nil, repeats: true)
         
         let sliderOrigin = self.view.bounds.size.width/2-280/2
-        let sliderDemo = UISlider(frame:CGRectMake(sliderOrigin, 550, 280, 20))
+        let sliderYAxis = self.view.bounds.size.height/2+260/2
+        let sliderDemo = UISlider(frame:CGRect(x: sliderOrigin, y: sliderYAxis, width: 280, height: 20))
         sliderDemo.minimumValue = 0
         sliderDemo.maximumValue = 100
-        sliderDemo.continuous = true
-        sliderDemo.tintColor = UIColor.redColor()
+        sliderDemo.isContinuous = true
+        sliderDemo.tintColor = UIColor.red
         sliderDemo.value = 50
-        sliderDemo.addTarget(self, action: #selector(ViewController.sliderValueDidChange(_:)), forControlEvents: .ValueChanged)
+        label?.text = "50.0"
+        sliderDemo.addTarget(self, action: #selector(ViewController.sliderValueDidChange(_:)), for: .valueChanged)
         self.view.addSubview(sliderDemo)
         
     }
     
-    func sliderValueDidChange(sender:UISlider!)
+    func sliderValueDidChange(_ sender:UISlider!)
     {
         print("value--\(sender.value)")
+        label?.text = sender.value.description
         circle.radian = sender.value/100
     }
 
@@ -87,9 +91,9 @@ class hand : UIView {
         super.init(coder: aDecoder)!
     }
 
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         
-        let c :CGContextRef = UIGraphicsGetCurrentContext()!
+        let c :CGContext = UIGraphicsGetCurrentContext()!
         let red : [CGFloat] = [1.0, 0.0, 0.0, 1.0]
         let pi = Float(M_PI)
         let degree : Float = (radian * pi) / 180
@@ -104,11 +108,11 @@ class hand : UIView {
         pointX1 += pointX;
         pointY1 += pointY;
         
-        CGContextSetStrokeColor(c, red);
-        CGContextBeginPath(c);
-        CGContextSetLineWidth(c, 3.0)
-        CGContextMoveToPoint(c, CGFloat(pointX), CGFloat(pointY));
-        CGContextAddLineToPoint(c, CGFloat(pointX1), CGFloat(pointY1));
-        CGContextStrokePath(c);
+        c.setStrokeColor(red);
+        c.beginPath();
+        c.setLineWidth(3.0)
+        c.move(to: CGPoint(x: CGFloat(pointX), y: CGFloat(pointY)));
+        c.addLine(to: CGPoint(x: CGFloat(pointX1), y: CGFloat(pointY1)));
+        c.strokePath();
     }
 }
